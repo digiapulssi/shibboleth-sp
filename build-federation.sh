3cf4f9a65aa32f859d7539fade38d15a3422bf01
Ville Valtonen <ville.valtonen@digia.com>
2017-02-01 14:20:22 +0200

    Fixed typo and linebreaks to linux format

--- build-federation.sh
+++ build-federation.sh
@@ -5,7 +5,7 @@ cat <<EOF > /etc/shibboleth-ds/federation-metadata.xml
 <EntitiesDescriptor xmlns="urn:oasis:names:tc:SAML:2.0:metadata" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:shibmd="urn:mace:shibboleth:metadata:1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" Name="https://your-federation.org/metadata/federation-name.xml">
 EOF
 
-if sed -i '/<\?.*?>/d' /etc/shibboleth-ds/* ; then
+if sed -i '/<\?.*?>/d' /etc/shibboleth-ds/idp_metadata/* ; then
   echo "Federation sources cleaned up"
 fi
 
